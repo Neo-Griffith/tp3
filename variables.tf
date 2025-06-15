ba801f2f2adde641c8c841c092999604843f8c9e
Corentin Leriche <azere49@gmail.com>
2025-06-15 15:20:57 +0200

    resultat terraform fmt

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "instance_type" {
-  type = string
+  type        = string
   description = "Set AWS EC2 instance type"
-  default = "t2.nano"
+  default     = "t2.nano"
 }
 
 variable "aws_common_tag" {
-  type = map(string)
+  type        = map(string)
   description = "Set AWS tag"
   default = {
     Name = "ec2-coco"
